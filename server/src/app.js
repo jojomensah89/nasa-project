cc6b033152c44449a794584e5078e811cf6f9331
jojomensah89 <jojomensah89@gmail.com>
2023-10-03 11:30:03 +0000

    added API versioning

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const v1Router = require("./routes/v1Router");
 
 const app = express();
 
@@ -16,8 +15,10 @@ app.use(
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use("/planets",planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", v1Router);
+
+// app.use("/v1/planets", planetsRouter);
+// app.use("/v1/launches", launchesRouter);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
